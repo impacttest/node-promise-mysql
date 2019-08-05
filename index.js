ff9b324ddd53824b58c84e431e51ae321fad81e7
Kenny Riddile <kriddile@impacttest.com>
2019-08-05 08:10:55 -0400

    create wrapper class for raw DB connections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Connection = require('./lib/connection.js');
 const Pool = require('./lib/pool.js');
-const PoolConnection = require('./lib/poolConnection.js');
+const RawConnectionWrapper = require('./lib/rawConnectionWrapper.js');
 const mysql = require('mysql');
 
 exports.createConnection = config => new Connection(config);
@@ -10,17 +10,7 @@ exports.createPool = config => new Pool(config);
 // Accepts a "raw" callback-based Connection or PoolConnection object from the "mysql" package and returns a
 // Connection object from this package that wraps that object in a Promise-enabled interface.  The returned object
 // should just be used for temporary compatibility with Promise-based code and not for managing database connection lifetime.
-exports.promisifyRawConnection = rawDatabaseConnection =>
-{
-    const wrapper = new PoolConnection( rawDatabaseConnection );
-
-    // Remove connection lifetime management functions
-    delete wrapper.release;
-    delete wrapper.destroy;
-    delete wrapper.end;
-
-    return wrapper;
-};
+exports.promisifyRawConnection = rawDatabaseConnection => new RawConnectionWrapper( rawDatabaseConnection );
 
 exports.Types = mysql.Types;
 exports.escape = mysql.escape;
