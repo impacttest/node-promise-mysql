da2b43b3748f80069030cda2898f59bd0bae8acd
Kenny Riddile <kriddile@impacttest.com>
2019-08-05 07:53:01 -0400

    remove database connection lifetime management functions from promisifyRawConnection()'s return value

--- index.js
+++ index.js
@@ -8,9 +8,19 @@ exports.createConnection = config => new Connection(config);
 exports.createPool = config => new Pool(config);
 
 // Accepts a "raw" callback-based Connection or PoolConnection object from the "mysql" package and returns a
-// PoolConnection object from this package that wraps that object in a Promise-enabled interface.  The returned object
+// Connection object from this package that wraps that object in a Promise-enabled interface.  The returned object
 // should just be used for temporary compatibility with Promise-based code and not for managing database connection lifetime.
-exports.promisifyRawConnection = rawDatabaseConnection => new PoolConnection( rawDatabaseConnection );
+exports.promisifyRawConnection = rawDatabaseConnection =>
+{
+    const wrapper = new PoolConnection( rawDatabaseConnection );
+
+    // Remove connection lifetime management functions
+    delete wrapper.release;
+    delete wrapper.destroy;
+    delete wrapper.end;
+
+    return wrapper;
+};
 
 exports.Types = mysql.Types;
 exports.escape = mysql.escape;
